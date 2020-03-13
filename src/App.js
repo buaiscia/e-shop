9d260cbd366981d758ca0180355378a2dc001785
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-03-13 16:23:42 +0100

    added collection preview first draft

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -20,8 +15,8 @@ function App() {
           component={HomePage}
         />
         <Route
-          path="/hats"
-          component={HatsPage}
+          path="/shop"
+          component={ShopPage}
         />
       </Switch>
     </div>
