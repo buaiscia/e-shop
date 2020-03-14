78b5a6a1631d4961f4c2b189fa41e13c45cdf5ef
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-03-14 20:19:25 +0100

    added sign in component and draft form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -19,6 +20,10 @@ function App() {
         <Route
           path="/shop"
           component={ShopPage}
+        />
+        <Route
+          path="/signin"
+          component={SignInAndSignUpPage}
         />
       </Switch>
     </div>
