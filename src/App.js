e65bc5e89953da55642ee597b26dddda6e2015aa
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-03-16 14:56:42 +0100

    added sign in with no gooogle

--- src/App.js
+++ src/App.js
@@ -31,9 +31,7 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          });
-          console.log(this.state);
-          
+          });        
         });
       }
       else {
